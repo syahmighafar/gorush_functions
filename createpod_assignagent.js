fef4debbace4233e6d453ec09fe34371811b83bc
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2022-01-11 14:50:20 +0800

    Update createpod_assignagent.js

--- createpod_assignagent.js
+++ createpod_assignagent.js
@@ -1490,6 +1490,39 @@ var countBarcode = 1;
                 document.getElementById("excelbuttonarea").style.display = 'block';
                 document.getElementById("assigntaskbuttonarea").style.display = 'block';
             }
+                
+                document.getElementById("downloadExl").addEventListener("click", exportTableToExcel('tasklisttable'));
+
+        function exportTableToExcel(tasklisttable, filename = '') {
+            var downloadLink;
+            var dataType = 'application/vnd.ms-excel';
+            var tableSelect = document.getElementById(tasklisttable);
+            var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
+
+            // Specify file name
+            filename = filename ? filename + '.xls' : 'pod_template.xls';
+
+            // Create download link element
+            downloadLink = document.createElement("a");
+
+            document.body.appendChild(downloadLink);
+
+            if (navigator.msSaveOrOpenBlob) {
+                var blob = new Blob(['\ufeff', tableHTML], {
+                    type: dataType
+                });
+                navigator.msSaveOrOpenBlob(blob, filename);
+            } else {
+                // Create a link to the file
+                downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+
+                // Setting the file name
+                downloadLink.download = filename;
+
+                //triggering the function
+                downloadLink.click();
+            }
+        }
 
             function assignTask() {
                 document.getElementById("inputarea").style.display = 'none';

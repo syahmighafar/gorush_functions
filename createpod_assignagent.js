3f3c9bbd1154617bd9564b221c7fe3d7bd079ee4
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2022-01-11 15:25:01 +0800

    Update createpod_assignagent.js

--- createpod_assignagent.js
+++ createpod_assignagent.js
@@ -1455,8 +1455,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
     document.getElementById("createPODTemplateButton").addEventListener("click", createPODTemplate);
 
     document.getElementById("submitAssignButton").addEventListener("click", assignTask);
-    
-    document.getElementById("downloadExl").addEventListener("click", exportTableToExcel);
 
     function createPODTemplate() {
 
@@ -1491,37 +1489,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
         document.getElementById("inputarea").style.display = 'block';
         document.getElementById("excelbuttonarea").style.display = 'block';
         document.getElementById("assigntaskbuttonarea").style.display = 'block';
-    }
-
-    function exportTableToExcel(tasklisttable, filename = '') {
-        var downloadLink;
-        var dataType = 'application/vnd.ms-excel';
-        var tableSelect = document.getElementById(tasklisttable);
-        var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
-
-        // Specify file name
-        filename = filename ? filename + '.xls' : 'pod_template.xls';
-
-        // Create download link element
-        downloadLink = document.createElement("a");
-
-        document.body.appendChild(downloadLink);
-
-        if (navigator.msSaveOrOpenBlob) {
-            var blob = new Blob(['\ufeff', tableHTML], {
-                type: dataType
-            });
-            navigator.msSaveOrOpenBlob(blob, filename);
-        } else {
-            // Create a link to the file
-            downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
-
-            // Setting the file name
-            downloadLink.download = filename;
-
-            //triggering the function
-            downloadLink.click();
-        }
     }
 
     function assignTask() {
